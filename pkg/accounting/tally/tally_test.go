5b25d08ba121ba4609ac4f414e039781d3bfa6cb
Kishore <kishore@storj.io>
2019-01-23 14:58:38 -0500

    fixed pkg/accounting test cases

--- pkg/accounting/tally/tally_test.go
+++ pkg/accounting/tally/tally_test.go
@@ -21,6 +21,7 @@ import (
 	"storj.io/storj/pkg/overlay/mocks"
 	"storj.io/storj/pkg/pb"
 	"storj.io/storj/pkg/pointerdb"
+	"storj.io/storj/pkg/uplinkdb"
 	"storj.io/storj/satellite/satellitedb"
 	"storj.io/storj/storage/teststore"
 )
@@ -60,6 +61,7 @@ func TestQueryWithBw(t *testing.T) {
 	assert.NoError(t, db.CreateTables())
 
 	bwDb := db.BandwidthAgreement()
+	uplDb := db.UplinkDB()
 	tally := tally.New(zap.NewNop(), db.Accounting(), bwDb, service, overlayServer, 0, time.Second)
 
 	//get a private key
@@ -68,20 +70,20 @@ func TestQueryWithBw(t *testing.T) {
 	k, ok := fiC.Key.(*ecdsa.PrivateKey)
 	assert.True(t, ok)
 
-	makeBWA(ctx, t, bwDb, "1", k, pb.PayerBandwidthAllocation_PUT)
-	makeBWA(ctx, t, bwDb, "2", k, pb.PayerBandwidthAllocation_GET)
-	makeBWA(ctx, t, bwDb, "3", k, pb.PayerBandwidthAllocation_GET_AUDIT)
-	makeBWA(ctx, t, bwDb, "4", k, pb.PayerBandwidthAllocation_GET_REPAIR)
-	makeBWA(ctx, t, bwDb, "5", k, pb.PayerBandwidthAllocation_PUT_REPAIR)
+	makeBWA(ctx, t, uplDb, bwDb, "1", k, pb.PayerBandwidthAllocation_PUT)
+	makeBWA(ctx, t, uplDb, bwDb, "2", k, pb.PayerBandwidthAllocation_GET)
+	makeBWA(ctx, t, uplDb, bwDb, "3", k, pb.PayerBandwidthAllocation_GET_AUDIT)
+	makeBWA(ctx, t, uplDb, bwDb, "4", k, pb.PayerBandwidthAllocation_GET_REPAIR)
+	makeBWA(ctx, t, uplDb, bwDb, "5", k, pb.PayerBandwidthAllocation_PUT_REPAIR)
 
 	//check the db
 	err = tally.QueryBW(ctx)
 	assert.NoError(t, err)
 }
 
-func makeBWA(ctx context.Context, t *testing.T, bwDb bwagreement.DB, serialNum string, k *ecdsa.PrivateKey, action pb.PayerBandwidthAllocation_Action) {
+func makeBWA(ctx context.Context, t *testing.T, upldb uplinkdb.DB, bwDb bwagreement.DB, serialNum string, k *ecdsa.PrivateKey, action pb.PayerBandwidthAllocation_Action) {
 	//generate an agreement with the key
-	pba, err := test.GeneratePayerBandwidthAllocation(action, k, k, time.Hour)
+	pba, err := test.GeneratePayerBandwidthAllocation(ctx, upldb, action, k, k, time.Hour)
 	assert.NoError(t, err)
 	rba, err := test.GenerateRenterBandwidthAllocation(pba, teststorj.NodeIDFromString("StorageNodeID"), k)
 	assert.NoError(t, err)
